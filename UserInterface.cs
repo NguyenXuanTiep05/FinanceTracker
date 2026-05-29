bcf36c84bdb1d0e26d3a703c35fa86d8fb6f37a5
Nguyen Xuan Tiep <nguyenpetr2005@gmail.com>
2026-05-29 23:19:45 +0200

    overview and adding expenses

--- UserInterface.cs
+++ UserInterface.cs
@@ -18,6 +18,10 @@ namespace FinanceTracker
 	{
 		private static readonly string[] _mainMenuOpt = ["Overview", "Add record"];
 		private static int _selOption = 0;
+        private static string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".FinanceData", "data.txt");
+
+
+
 		static int Mod(int a, int b) => ((a % b) + b) % b;
 		public static void DrawMainMenu(int selected)
 		{
@@ -38,15 +42,47 @@ namespace FinanceTracker
 			switch (_selOption)
 			{
 				case 0:
-					Console.WriteLine("Overview");
+                    Overview();
 				break;
 				case 1:
-					Console.WriteLine("Add");
+                    AddRecord();
 				break;
 			}
-			Console.ReadKey();
 		}
+
+
+
+        private static void AddRecord(){
+            Console.WriteLine("Write Expense:\t{value / note}");
+            Console.CursorVisible = true;
+            Console.WriteLine(filePath);
+            string? record = Console.ReadLine();
+            Console.CursorVisible = false;
+            if (record == ""){return;}
+
+            string[] data = record.Split("/");
+            double val;
+            string value = double.TryParse(data[0],out val) ? data[0] : "";
+            string note = data.Length > 1 ? data[1] : "";
+            if (value == ""){return;}
+            string output = $"{DateTime.Now.ToString()} | {value} | {note}";
+
+            Directory.CreateDirectory(Path.GetDirectoryName(filePath)!);
+            File.AppendAllText(filePath,output + Environment.NewLine);
+        }
+
+        private static void Overview(){
+            Directory.CreateDirectory(Path.GetDirectoryName(filePath)!);
+            string[] lines = File.ReadAllLines(filePath);           
+            foreach(string line in lines){
+                Console.WriteLine(line);
+            }
+
+            Console.ReadKey();
+        }
+
 	}
+
 }
 
 
