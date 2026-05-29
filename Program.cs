00f1925cfd75a3591e32127ee2d7170bc7fc7dc1
NguyenXuanTiep05 <NguyenPetr2005@gmail.com>
2026-05-29 11:38:35 +0200

    added UserInterface

--- Program.cs
+++ Program.cs
@@ -13,17 +13,34 @@
 
 
 using System;
-using FinanceTracker;
 
-static void Main()
+namespace FinanceTracker
 {
-	Console.WriteLine
+    class Program
+    {
+        static void Main()
+        {
+
+    		int selected = 0;
+            while (true)
+            {
+    			UserInterface.DrawMainMenu(selected);
+                ConsoleKeyInfo key = Console.ReadKey(false);
+                switch (key.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        selected++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        selected--;
+                        break;
+                }
+            }
+
+        }
+    }
 }
 
-
-
-
-Main();
 
 
 
--- UserInterface.cs
+++ UserInterface.cs
@@ -16,7 +16,20 @@ namespace FinanceTracker
 {
 	public static class UserInterface
 	{
-		
+		private static readonly string[] _mainMenuOpt = {"Option 1", "Option 2", "Option 3", "Option 4"};
+		private readonly static int _selOption;
+		static int Mod(int a, int b) => ((a % b) + b) % b;
+		public static void DrawMainMenu(int selected)
+		{
+			Console.Clear();
+			for (int i = 0; i < _mainMenuOpt.Length; i++)
+			{
+				string space = " ";
+				if (i == Mod(selected, _mainMenuOpt.Length)){space = ">";}
+				string option =space + _mainMenuOpt[i];
+				Console.WriteLine(option);
+			}
+		}
 	}
 }
 
